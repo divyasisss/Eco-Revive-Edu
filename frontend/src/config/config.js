f97177afa1d9c7b1baf43e4b1d9cc43c6fba433a
Divya Dwivedi <divyadwivedi.6616@gmail.com>
2024-10-14 21:06:50 +0530

    Update config.js

--- frontend/src/config/config.js
+++ frontend/src/config/config.js
@@ -1,6 +1,6 @@
 const config = {
     // apiUrl: 'http://localhost:8080/api', // Example URL; adjust as needed
-    apiUrl: 'eco-revive-edu.vercel.app'
+    apiUrl: 'eco-revive-edu.vercel.app/api'
   };
   
 export default config;
